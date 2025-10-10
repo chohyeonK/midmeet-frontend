11ed0c4ccfdcd30cfc708ed2acb216af88503003
Chohyeon Kim <chohyeonK3@gmail.com>
2025-10-10 21:51:39 +0900

    feat: 모임 시작 페이지 개발중 - 모임원 정보 저장 기능 마무리 해야함

--- src/components/forms/JoinForm.tsx
+++ src/components/forms/JoinForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, type BaseSyntheticEvent } from 'react';
 import Label from '../common/Label';
 import Input from '../common/Input';
 import Button from '../common/Button';
@@ -6,29 +6,35 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm, type FieldErrors, type SubmitHandler, type UseFormReturn } from 'react-hook-form';
 import { partyJoinInputSchema } from '../../validation/authSchema';
+import AddressSearchMap from '../party/AddressSearchMap';
 
 type FormData = yup.InferType<typeof partyJoinInputSchema>;
+// type SetAddressAndCoords = (addressFieldName: string, addressValue: string, lat: number, lng: number);
 
 interface JoinFormProps {
   title: string;
   subTitle: string;
-  onSubmit: SubmitHandler<FormData>;
+  onSubmit: (e?: BaseSyntheticEvent) => Promise<void>;
   register: UseFormReturn<FormData>['register'];
-  handleSubmit: UseFormReturn<FormData>['handleSubmit'];
   errors: FieldErrors<FormData>;
+  setAddressAndCoords: (addressFieldName: string, addressValue: string) => void;
 }
 
-const JoinForm: React.FC<JoinFormProps> = ({ onSubmit, register, handleSubmit, errors }) => {
+const JoinForm: React.FC<JoinFormProps> = ({ onSubmit, register, errors, setAddressAndCoords }) => {
   return (
     <div className='flex flex-col items-center py-12 px-4'>
-      <form onSubmit={handleSubmit(onSubmit)} className='w-full sm:max-w-md space-y-6 text-left'>
+      <form onSubmit={onSubmit} className='w-full sm:max-w-md space-y-6 text-left'>
         <h1 className='text-3xl font-bold mb-6 text-mint-500'>모임명</h1>
         <h1 className='text-2xl font-bold mb-6'>모임원 정보 입력하기</h1>
 
         <div className='space-y-2'>
-          <Label htmlFor='from' LabelName='출발지 입력' />
-          <Input type='text' name='from' placeholder='시/군/구 (예: 서울 강남구)' register={register} error={errors.from} />
-          {/* 지도 api 연동 */}
+          {/* <Label htmlFor='from' LabelName='출발지 입력' />
+          <div className='flex'>
+            <Input type='text' name='from' placeholder='시/군/구 (예: 서울 강남구)' register={register} error={errors.from} isFullWidth={false} className='grow-5 mr-3' />
+            <Button type='button' buttonName='검색' className='w-28 flex-shrink-0 bg-indigo-600' onClick={handleModalOpen} />
+          </div>
+          <div ref={mapRef} className='w-full h-64 bg-gray-200 rounded-lg border'></div> */}
+          <AddressSearchMap setAddressAndCoords={setAddressAndCoords} addressFieldName='from' latFieldName='startLatitude' lngFieldName='startLongitude' />
         </div>
 
         <div className='space-y-2'>
@@ -39,8 +45,8 @@ const JoinForm: React.FC<JoinFormProps> = ({ onSubmit, register, handleSubmit, e
             {...register('transportation')}
           >
             <option value=''>교통수단을 선택해주세요.</option>
-            <option value='public'>대중교통</option>
-            <option value='private'>자가</option>
+            <option value='PUBLIC'>대중교통</option>
+            <option value='PRIVATE'>자가</option>
           </select>
           {errors.transportation && <p className='text-red-500 text-sm mt-1'>{errors.transportation.message}</p>}
         </div>
